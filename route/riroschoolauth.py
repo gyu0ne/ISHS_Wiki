ebdd5bbe337bada8bc7a42e5144778c52e474d06
LDH <aaddaamm0615@gmail.com>
2025-09-25 09:19:05 +0900

    riroschoolauth 수정

--- route/riroschoolauth.py
+++ route/riroschoolauth.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Form
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import requests, time
 from bs4 import BeautifulSoup
@@ -10,8 +10,8 @@ app.add_middleware(
     allow_methods=["*"], allow_headers=["*"],
 )
 
-@app.post("/api/riro_login")
-def riro_login(id: str = Form(...), password: str = Form(...)):
+@app.get("/api/riro_login")
+def riro_login(id: str, password: str):
     s = requests.Session()
     headers = {
         "Content-Type": "application/x-www-form-urlencoded",
@@ -19,13 +19,12 @@ def riro_login(id: str = Form(...), password: str = Form(...)):
     }
 
     SLEEP_SEC = 2
-    MAX_RETRIES = 3
-    last_exception = None
+    MAX_RETRIES = 5
 
     for _ in range(MAX_RETRIES):
         try:
             try:
-                s.get("https://iscience.riroschool.kr/user.php?action=user_logout", timeout=10)
+                s.post("https://iscience.riroschool.kr/user.php?action=user_logout", timeout=10)
             except requests.RequestException:
                 pass
 
@@ -65,45 +64,81 @@ def riro_login(id: str = Form(...), password: str = Form(...)):
             html = r2.text
             soup = BeautifulSoup(html, "html.parser")
 
-            el_student = soup.select_one("span.m_level3")
-            if not el_student:
-                el_student = soup.select_one("span.m_level1")
-            inputs = soup.select(".input_disabled")
+            account_type = "normal"
+            if soup.select(".td_title")[0].get_text() == "통합아이디":
+                account_type = "integrated"
 
-            if not el_student or len(inputs) < 2:
-                raise RuntimeError("Cannot parse user info")
+            if account_type == "normal":
+                el_student = soup.select_one("span.m_level3")
+                if not el_student:
+                    el_student = soup.select_one("span.m_level1")
+                inputs = soup.select(".input_disabled")
 
-            student = el_student.get_text(strip=True) or ""
-            name = (inputs[0].get_text(strip=True) or "")
-            student_number_raw = (inputs[1].get_text(strip=True) or "")
+                if not el_student or len(inputs) < 2:
+                    raise RuntimeError("Cannot parse user info")
 
-            if len(student_number_raw) >= 3:
-                student_number = student_number_raw[0] + student_number_raw[2:]
-            else:
-                student_number = student_number_raw
+                student = el_student.get_text(strip=True) or ""
+                name = (inputs[0].get_text(strip=True) or "")
+                student_number_raw = (inputs[1].get_text(strip=True) or "")
 
-            generation = 0
-            if len(id) >= 2 and id[:2].isdigit():
-                generation = int("20" + id[:2]) - 1994 + 1
+                if len(student_number_raw) >= 3:
+                    student_number = student_number_raw[0] + student_number_raw[2:]
+                else:
+                    student_number = student_number_raw
 
-            if all([name, student_number, student]) and generation > 0:
-                return {
-                    "status": "success",
-                    "name": name,
-                    "student_number": student_number,
-                    "generation": generation,
-                    "student": student,
-                }
+                generation = 0
+                if len(id) >= 2 and id[:2].isdigit():
+                    generation = int("20" + id[:2]) - 1994 + 1
+
+                if all([name, student_number, student]) and generation > 0:
+                    return {
+                        "status": "success",
+                        "name": name,
+                        "student_number": student_number,
+                        "generation": generation,
+                        "student": student,
+                    }
+
+            elif account_type == "integrated":
+                riro_id = soup.select(".elem_fix")[0].get_text()[:8]
+                student = soup.select(".elem_fix")[0].get_text()[15:-1]
+
+                inputs = soup.select(".input_disabled")
+
+                name = (inputs[0].get_text(strip=True) or "")
+                student_number_raw = (inputs[1].get_text(strip=True) or "")
+
+                if len(student_number_raw) >= 3:
+                    student_number = student_number_raw[0] + student_number_raw[2:]
+                else:
+                    student_number = student_number_raw
+
+                generation = 0
+                if len(riro_id) >= 2 and riro_id[:2].isdigit():
+                    generation = int("20" + riro_id[:2]) - 1994 + 1
+
+                print(riro_id, student, name, student_number, generation)
+                if all([name, student_number, student]) and generation > 0:
+                    return {
+                        "status": "success",
+                        "name": name,
+                        "student_number": student_number,
+                        "generation": generation,
+                        "student": student,
+                    }
+                print("통합아이디")
 
             raise RuntimeError("Data missing. Retrying...")
 
         except (requests.RequestException, RuntimeError) as e:
-            last_exception = e
-            # print("Error:", e) # 콘솔 출력 제거
+            print("Error:", e)
             time.sleep(SLEEP_SEC)
-            
-    return {"status": "error", "message": f"인증 서버와 통신 중 오류가 발생했습니다."}
+
+    return {
+        "status": "error",
+        "message": "인증 서버와 통신 중 오류가 발생했습니다."
+    }
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=5001)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
