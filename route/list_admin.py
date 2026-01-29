60c438f416a4c43136f6a231fe115ee3152b616b
LDH <aaddaamm0615@gmail.com>
2026-01-30 01:55:30 +0900

    fix user setting and admin list

--- route/list_admin.py
+++ route/list_admin.py
@@ -11,7 +11,7 @@ async def list_admin():
         ))
         for data in curs.fetchall():
             name = '' + \
-                html.escape(await ip_pas(data[0])) + ' ' + \
+                await ip_pas(data[0]) + ' ' + \
                 '<a href="/auth/list/add/' + url_pas(data[1]) + '">(' + html.escape(data[1]) + ')</a>' + \
             ''
 
